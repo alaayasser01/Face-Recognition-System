ac140730e7ba5392077ca766535b3d9efa3e5f76
EHVB <ehab.mahmoud00@eng-st.cu.edu.eg>
2023-05-16 02:25:25 +0300

    model deployment

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st 
+import pickle
+import functions
+
+with open("model.pkl","rb") as f:
+        model = pickle.load(f)   
+
+st.set_page_config(layout="wide")
+
+def head():
+    st.markdown("""
+        <h1 style='text-align: center; margin-bottom: -35px;'>
+        Image Recognition App
+        </h1>
+    """, unsafe_allow_html=True
+                )
+
+    st.caption("""
+        <p style='text-align: center'>
+        by team 18
+        </p>
+    """, unsafe_allow_html=True
+               )
+
+def main():
+    image = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"]) 
+    
+    col1, col2 = st.columns(2)
+    
+    if image:
+        with col1:
+            st.image(image)
+        pred = functions.predictor(f"test/{image.name}",functions.mean_face, functions.eigvecs, model)
+        with col2:
+            st.success(pred)
+        
+if __name__ == '__main__':
+    head()
+    main()
+        
